fcd3b01002e0dd07d95cdaa9f31abc53b92768c3
Vadim Tkachenko <vt@homeclimatecontrol.com>
2014-08-21 00:07:28 -0700

    - Added instrumentation; - Added necessary dependencies.

---
+++ dz3-master/dz3-common/src/main/java/net/sf/dz3/instrumentation/Marker.java
@@ -0,0 +1,164 @@
+package net.sf.dz3.instrumentation;
+
+import org.apache.commons.lang3.time.StopWatch;
+import org.apache.log4j.Logger;
+
+/**
+ * An object to keep track of time spent on something in a convenient manner.
+ * 
+ * Usage pattern (just like {@link NDC}:
+ * 
+ * {@code
+ * 
+ * NDC.push("whatever");
+ * Marker m = new Marker("something complicated");
+ * 
+ * try {
+ *     ... do something
+ *  
+ * } finally {
+ * 
+ *     // NOTE: Put m.close() BEFORE NDC.pop(), not after, to preserve the context
+ *     m.close();
+ *     NDC.pop();
+ * }
+ * 
+ */
+public class Marker {
+
+    private final Logger logger = Logger.getLogger(getClass());
+    
+    /**
+     * Message to be printed in the log when the marker is closed.
+     */
+    private final String marker;
+    
+    /**
+     * Timekeeper.
+     */
+    private StopWatch stopWatch;
+    
+    /**
+     * Redundant invocation preventer.
+     */
+    private boolean closed = false;
+    
+    /**
+     * Create an instance and start the {@link #stopWatch timer}.
+     * 
+     * @param marker Message to print when the marker is closed.
+     * It would be a good idea not to use parentheses in this string, for they
+     * will break automated processing.
+     */
+    public Marker(String marker) {
+    
+        if (marker == null) {
+            throw new IllegalArgumentException("Marker can't be null");
+        }
+        
+        this.marker = marker;
+        
+        stopWatch = new StopWatch();
+        stopWatch.start();
+
+        printStartMarker();
+    }
+
+    protected void printStartMarker() {
+
+        // Mark the beginning of the timed section
+
+        StringBuilder sb = new StringBuilder();
+
+        getSignature(sb);
+        sb.append(marker).append(") started");
+
+        logger.info(sb.toString());
+    }
+    
+    /**
+     * Print the diagnostic message and keep going.
+     * 
+     * @param checkpointMessage Message to print.
+     * 
+     * @return Time elapsed since creation of this marker instance, in milliseconds.
+     */
+    public final long checkpoint(String checkpointMessage) {
+        
+        StringBuilder sb = new StringBuilder();
+        
+        getSignature(sb);
+        sb.append(marker);
+        sb.append(") checkpoint '" + checkpointMessage + "' reached at ");
+
+        printTimeMarker(sb, stopWatch);
+        
+        logger.info(sb.toString());
+        
+        return stopWatch.getTime();
+    }
+    
+    /**
+     * Close the interval and print the diagnostic message.
+     * 
+     * You can only {@code close()} the marker once.
+     * 
+     * @return Time elapsed since creation of this marker instance, in milliseconds.
+     */
+    public final long close() {
+        
+        if (closed) {
+            StringBuilder sb = new StringBuilder();
+            
+            getSignature(sb);
+            sb.append("already closed)");
+            throw new IllegalStateException(sb.toString());
+        }
+        
+        stopWatch.stop();
+
+        closed = true;
+        
+        StringBuilder sb = new StringBuilder();
+        
+        getSignature(sb);
+        sb.append(marker);
+        sb.append(") completed in ");
+
+        printTimeMarker(sb, stopWatch);
+        
+        logger.info(sb.toString());
+        
+        return stopWatch.getTime();
+    }
+
+    protected void printTimeMarker(StringBuilder sb, StopWatch stopWatch) {
+        sb.append(stopWatch.getTime()).append(" ms (").append(stopWatch.toString()).append(")");
+    }
+    
+    /**
+     * Get the unique marker signature.
+     * 
+     * @param sb Buffer to append the signature to.
+     */
+    private void getSignature(StringBuilder sb) {
+        sb.append("marker");
+        sb.append('@').append(Integer.toHexString(Thread.currentThread().hashCode()));
+        sb.append('#').append(Integer.toHexString(hashCode()));
+        sb.append(": (");
+    }
+    
+    public void finalize() {
+        
+        if (!closed) {
+
+            StringBuilder sb = new StringBuilder();
+
+            getSignature(sb);
+            sb.append(marker);
+            sb.append(") RUNAWAY INSTANCE, NOT close()d");
+
+            logger.error(sb.toString());
+        }
+    }
+}
