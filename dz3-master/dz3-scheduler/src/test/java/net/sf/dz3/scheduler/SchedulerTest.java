2e61f25edead3111e610dd573acff7991c1b0c2b
Vadim Tkachenko <vt@homeclimatecontrol.com>
2015-06-19 16:03:09 -0700

    Added a test case

--- dz3-master/dz3-scheduler/src/test/java/net/sf/dz3/scheduler/SchedulerTest.java
+++ dz3-master/dz3-scheduler/src/test/java/net/sf/dz3/scheduler/SchedulerTest.java
@@ -2,14 +2,31 @@ package net.sf.dz3.scheduler;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.Random;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
 import net.sf.dz3.device.model.Thermostat;
 import net.sf.dz3.device.model.ZoneStatus;
+import net.sf.dz3.scheduler.Scheduler.Deviation;
 import junit.framework.TestCase;
 
 public class SchedulerTest extends TestCase {
+    
+    private final Random rg = new Random();
+    
+    public void testDeviation() {
+        
+        double setpoint = rg.nextDouble();
+        boolean enabled = rg.nextBoolean();
+        boolean voting = rg.nextBoolean();
+        
+        Deviation d = new Deviation(setpoint, enabled, voting);
+        
+        assertEquals(setpoint,d.setpoint);
+        assertEquals(enabled, d.enabled);
+        assertEquals(voting, d.voting);
+    }
 
     public void testInstantiation() {
         
