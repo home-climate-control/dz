0bc3d6b6e8eb70fd006e5919a858b195f5132f61
Vadim Tkachenko <vt@homeclimatecontrol.com>
2015-06-24 22:05:25 -0700

    Fixed https://github.com/home-climate-control/dz/issues/7; added test case for it.

--- dz3-master/dz3-common/src/test/java/net/sf/dz3/util/counter/FileUsageCounterTest.java
+++ dz3-master/dz3-common/src/test/java/net/sf/dz3/util/counter/FileUsageCounterTest.java
@@ -10,29 +10,26 @@ import junit.framework.TestCase;
 
 public class FileUsageCounterTest extends TestCase {
 
-    public void testNonExistentFile() throws IOException {
+    /**
+     * Make sure that the persistent storage file that doesn't exist is created.
+     */
+    public void testNonExistentDirect() throws IOException {
         
-        File f = createNonexistent();
-        DataSource<Double> target = new DataSource<Double>() {
-
-            @Override
-            public void addConsumer(DataSink<Double> arg0) {
-                // TODO Auto-generated method stub
-                
-            }
-
-            @Override
-            public void removeConsumer(DataSink<Double> arg0) {
-                // TODO Auto-generated method stub
-                
-            }};
+        File f = createNonexistentDirect();
+        FileUsageCounter counter = new FileUsageCounter("test", new TimeBasedUsage(), createTarget(), f);
         
-        FileUsageCounter counter = new FileUsageCounter("test", new TimeBasedUsage(), target, f);
-        
+        assertEquals("The file mustn't exist at this point yet", false, f.exists());
         counter.save();
+        assertEquals("The file exist at this point", true, f.exists());
+        
+        f.delete();
+        assertEquals("The file must have been deleted by now", false, f.exists());
     }
 
-    private File createNonexistent() {
+    /**
+     * @return a file name in an existing directory.
+     */
+    private File createNonexistentDirect() {
 
         File tmp = new File(System.getProperty("java.io.tmpdir"));
         
@@ -42,5 +39,55 @@ public class FileUsageCounterTest extends TestCase {
         }
         
         return new File(tmp, UUID.randomUUID().toString());
+    }
+
+    public void testNonExistentFileIndirect() throws IOException {
+        
+        File f = createNonexistentIndirect();
+        FileUsageCounter counter = new FileUsageCounter("test", new TimeBasedUsage(), createTarget(), f);
+        
+        assertEquals("The file mustn't exist at this point yet", false, f.exists());
+        assertEquals("The file parent directory mustn't exist at this point yet", false, f.getParentFile().exists());
+        counter.save();
+        assertEquals("The file exist at this point", true, f.exists());
+
+        f.delete();
+        f.getParentFile().delete();
+
+        assertEquals("The file must have been deleted by now", false, f.exists());
+        assertEquals("The file parent directory must have been deleted by now", false, f.getParentFile().exists());
+    }
+
+    /**
+     * @return a file name in a non-existing directory.
+     */
+    private File createNonexistentIndirect() {
+
+        File tmp = new File(System.getProperty("java.io.tmpdir"));
+        
+        if (!tmp.exists() || !tmp.canWrite()) {
+            
+            fail(tmp + ": doesn't exist or can't write");
+        }
+        
+        File indirect = new File(tmp, UUID.randomUUID().toString());
+        
+        return new File(indirect, "oops");
+    }
+    
+    private DataSource<Double> createTarget() {
+        
+        return new DataSource<Double>() {
+
+            @Override
+            public void addConsumer(DataSink<Double> arg0) {
+                // Do absolutely nothing
+            }
+
+            @Override
+            public void removeConsumer(DataSink<Double> arg0) {
+                // Do absolutely nothing
+            }
+        };
     }
 }
