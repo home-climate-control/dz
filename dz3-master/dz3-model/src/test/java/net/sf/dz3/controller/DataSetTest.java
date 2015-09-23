3b2a1a9845fd8c92775cce2349c70b6145c1ca18
Vadim Tkachenko <vt@homeclimatecontrol.com>
2015-09-22 20:13:01 -0700

    Added a test case

--- dz3-master/dz3-model/src/test/java/net/sf/dz3/controller/DataSetTest.java
+++ dz3-master/dz3-model/src/test/java/net/sf/dz3/controller/DataSetTest.java
@@ -28,4 +28,27 @@ public class DataSetTest extends TestCase {
             assertEquals("Wrong exception message", "Data element out of sequence: last key is 101, key being added is 99", ex.getMessage());
         }
     }
+
+    public void testExpire() {
+        
+        DataSet<Double> ds = new DataSet<Double>(100);
+        
+        ds.record(0, 0d);
+        ds.record(100, 0d);
+        
+        assertEquals("Wrong value", 0, ds.iterator().next().longValue());
+
+        {
+            
+            // This value won't cause expiration
+            ds.record(100, 0d);
+            assertEquals("Wrong value before expiration", 0, ds.iterator().next().longValue());
+        }
+
+        {
+            // This value *will* cause expiration
+            ds.record(101, 0d);
+            assertEquals("Wrong value after expiration", 100, ds.iterator().next().longValue());
+        }
+    }
 }
