58ae425e596424f937c7b664d8bc68cb50cfe040
Vadim Tkachenko <vt@homeclimatecontrol.com>
2015-09-28 10:56:35 -0700

    Pinpointed the difference: results differ when there's more than one record expired at the time

--- dz3-master/dz3-model/src/test/java/net/sf/dz3/controller/IntegralSetTest.java
+++ dz3-master/dz3-model/src/test/java/net/sf/dz3/controller/IntegralSetTest.java
@@ -9,6 +9,9 @@ import net.sf.dz3.instrumentation.Marker;
 import org.apache.log4j.Logger;
 import org.apache.log4j.NDC;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.Semaphore;
 
@@ -116,6 +119,78 @@ public class IntegralSetTest extends TestCase {
             }
         
             m.close();
+            NDC.pop();
+        }
+
+    }
+
+    /**
+     * Make sure the slow and fast implementation yield the same results, step by step, with NO more than one record ever expired.
+     */
+    public void testSameSingleExpiration() {
+        
+        List<Long> timestamps = new LinkedList<Long>();
+
+        // Make sure no intervals exceed the expiration interval so no more than one entry ever needs to be expired
+        timestamps.add(80L);
+        timestamps.add(80L);
+        timestamps.add(80L);
+        timestamps.add(80L);
+        timestamps.add(80L);
+        timestamps.add(80L);
+        
+        testSameSteps(100, timestamps);
+    }
+
+    /**
+     * Make sure the slow and fast implementation yield the same results, step by step, with MORE than one record ever expired.
+     */
+    public void testSameMultipleExpiration() {
+        
+        List<Long> timestamps = new LinkedList<Long>();
+
+        timestamps.add(80L);
+        timestamps.add(80L);
+        timestamps.add(80L);
+        
+        // This difference will trigger expiration for more than one record
+        timestamps.add(300L);
+        
+        timestamps.add(80L);
+        timestamps.add(80L);
+        
+        testSameSteps(100, timestamps);
+    }
+
+    /**
+     * Make sure the slow and fast implementation yield the same results.
+     */
+    public void testSameSteps(long expirationInterval, List<Long> timestamps) {
+
+        NDC.push("testSameSteps/I");
+
+        try {
+
+            IntegralSet dataSet2000 = new LegacyIntegralSet(expirationInterval);
+            IntegralSet dataSet2015 = new NaiveIntegralSet(expirationInterval);
+
+            long timestamp = 0;
+
+            for (Iterator<Long> i = timestamps.iterator(); i.hasNext(); ) {
+
+                timestamp += i.next();
+                double value = rg.nextDouble();
+
+                dataSet2000.record(timestamp, value);
+                dataSet2015.record(timestamp, value);
+                
+                logger.info("timestamp/expiration: " + timestamp + "/" + expirationInterval);
+
+                assertEquals(dataSet2000.getIntegral(), dataSet2015.getIntegral());
+            }
+
+        } finally {
+            
             NDC.pop();
         }
 
