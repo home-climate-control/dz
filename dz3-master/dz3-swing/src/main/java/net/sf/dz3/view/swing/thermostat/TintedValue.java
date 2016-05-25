dd12dbea92cf823244b675dcee89191640b62a8f
Vadim Tkachenko <vt@homeclimatecontrol.com>
2016-05-25 00:54:07 -0700

    Replaced tabs with spaces (how/when did they get in?)

--- dz3-master/dz3-swing/src/main/java/net/sf/dz3/view/swing/thermostat/TintedValue.java
+++ dz3-master/dz3-swing/src/main/java/net/sf/dz3/view/swing/thermostat/TintedValue.java
@@ -3,27 +3,27 @@ package net.sf.dz3.view.swing.thermostat;
 /**
  * Intended to hold a data sample for the {@link Chart variable color chart}.
  * 
- * @author Copyright &copy; <a href="mailto:vt@freehold.crocodile.org">Vadim Tkachenko</a> 2001-2012
+ * @author Copyright &copy; <a href="mailto:vt@freehold.crocodile.org">Vadim Tkachenko</a> 2001-2016
  */
 public class TintedValue {
 
-	public final double value;
-	public final double tint;
-	
-	public TintedValue(double value, double tint) {
-		
-		this.value = value;
-		this.tint = tint;
-	}
-	
-	@Override
-	public String toString() {
-		
-		StringBuilder sb = new StringBuilder();
-		
-		sb.append("TintedValue(").append(value).append(", ").append(tint);
-		sb.append(")");
-		
-		return sb.toString();
-	}
+    public final double value;
+    public final double tint;
+
+    public TintedValue(double value, double tint) {
+
+        this.value = value;
+        this.tint = tint;
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("TintedValue(").append(value).append(", ").append(tint);
+        sb.append(")");
+
+        return sb.toString();
+    }
 }
