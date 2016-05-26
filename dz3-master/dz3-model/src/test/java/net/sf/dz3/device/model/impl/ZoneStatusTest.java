922bbbf60068f43fa199acee8ee5d2dad3ece2e8
Vadim Tkachenko <vt@homeclimatecontrol.com>
2016-05-26 15:47:09 -0700

    Investigating #13, improving test coverage on ZoneStatusImpl

--- dz3-master/dz3-model/src/test/java/net/sf/dz3/device/model/impl/ZoneStatusTest.java
+++ dz3-master/dz3-model/src/test/java/net/sf/dz3/device/model/impl/ZoneStatusTest.java
@@ -2,6 +2,7 @@ package net.sf.dz3.device.model.impl;
 
 import java.util.Random;
 
+import net.sf.dz3.device.model.ZoneStatus;
 import junit.framework.TestCase;
 
 public class ZoneStatusTest extends TestCase {
@@ -20,5 +21,113 @@ public class ZoneStatusTest extends TestCase {
             
             assertNotSame("Double comparison failure", status1, status2);
         }
+    }
+    
+    public void testNegativeDumpPriority() {
+
+        try {
+        
+            new ZoneStatusImpl(0, -1, true, true);
+            fail("Should've failed by now");
+        
+        } catch (IllegalArgumentException ex) {
+            assertEquals("wrong exception message", "Dump priority must be non-negative (-1 given)", ex.getMessage());
+        }
+    }
+
+    public void testEqualsNull() {
+
+        assertFalse("Improper null comparison", new ZoneStatusImpl(0, 0, true, true).equals(null));
+    }
+
+    public void testEqualsAlien() {
+
+        ZoneStatus zs = new ZoneStatusImpl(0, 0, true, true);
+
+        assertFalse("Improper null comparison", zs.equals(zs.toString()));
+    }
+
+    public void testEqualsThermostatStatus() {
+
+        ZoneStatus a = new ZoneStatusImpl(0, 0, true, true);
+        ZoneStatus b = new ThermostatStatusImpl(0, 0, 0, true, true, true, false);
+
+        assertTrue("Improper heterogenous comparison", a.equals(b));
+    }
+
+    public void testEqualsSame() {
+
+        ZoneStatus a = new ZoneStatusImpl(0, 0, true, true);
+        ZoneStatus b = new ZoneStatusImpl(0, 0, true, true);
+
+        assertTrue("Improper comparison", a.equals(b));
+    }
+
+    public void testDifferentSetpoint() {
+
+        ZoneStatus a = new ZoneStatusImpl(0, 0, true, true);
+        ZoneStatus b = new ZoneStatusImpl(1, 0, true, true);
+
+        assertFalse("Improper comparison", a.equals(b));
+    }
+
+    public void testDifferentDump() {
+
+        ZoneStatus a = new ZoneStatusImpl(0, 0, true, true);
+        ZoneStatus b = new ZoneStatusImpl(0, 1, true, true);
+
+        assertFalse("Improper comparison", a.equals(b));
+    }
+
+    public void testDifferentEnabled() {
+
+        ZoneStatus a = new ZoneStatusImpl(0, 0, true, true);
+        ZoneStatus b = new ZoneStatusImpl(0, 0, false, true);
+
+        assertFalse("Improper comparison", a.equals(b));
+    }
+
+    public void testDifferentVoting() {
+
+        ZoneStatus a = new ZoneStatusImpl(0, 0, true, true);
+        ZoneStatus b = new ZoneStatusImpl(0, 0, true, false);
+
+        assertFalse("Improper comparison", a.equals(b));
+    }
+
+    public void testHashCodeEquals() {
+
+        ZoneStatus a = new ZoneStatusImpl(0, 0, true, true);
+        ZoneStatus b = new ZoneStatusImpl(0, 0, true, true);
+
+        assertTrue("Improper hashcode comparison", a.hashCode() == b.hashCode());
+    }
+
+    public void testHashCodeDiffers() {
+
+        ZoneStatus a = new ZoneStatusImpl(0, 0, true, true);
+        ZoneStatus b = new ZoneStatusImpl(1, 0, true, true);
+
+        assertFalse("Improper hashcode comparison", a.hashCode() == b.hashCode());
+    }
+
+    public void testToString0011() {
+
+        assertEquals("Wrong string representation", new ZoneStatusImpl(0, 0, true, true).toString(), "setpoint=0.0, enabled, voting");
+    }
+
+    public void testToString0111() {
+
+        assertEquals("Wrong string representation", new ZoneStatusImpl(0, 1, true, true).toString(), "setpoint=0.0, enabled, voting, dump priority=1");
+    }
+
+    public void testToString0101() {
+
+        assertEquals("Wrong string representation", new ZoneStatusImpl(0, 1, false, true).toString(), "setpoint=0.0, disabled, voting, dump priority=1");
+    }
+
+    public void testToString0110() {
+
+        assertEquals("Wrong string representation", new ZoneStatusImpl(0, 1, true, false).toString(), "setpoint=0.0, enabled, not voting, dump priority=1");
     }
 }
