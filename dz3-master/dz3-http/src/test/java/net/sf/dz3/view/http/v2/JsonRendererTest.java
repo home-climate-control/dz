459b5fa0c63f9fbebce27d1668d5bbb416a35290
Vadim Tkachenko <vt@seriousinsights.com>
2017-11-09 02:13:20 -0700

    Added a rudimentary test case

--- dz3-master/dz3-http/src/test/java/net/sf/dz3/view/http/v2/JsonRendererTest.java
+++ dz3-master/dz3-http/src/test/java/net/sf/dz3/view/http/v2/JsonRendererTest.java
@@ -1,11 +1,54 @@
 package net.sf.dz3.view.http.v2;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.assertEquals;
 
-public class JsonRendererTest extends TestCase {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.NDC;
+import org.junit.Test;
+
+import net.sf.dz3.device.model.ThermostatSignal;
+import net.sf.dz3.device.model.impl.ThermostatModel;
+import net.sf.dz3.device.sensor.impl.NullSensor;
+import net.sf.dz3.view.http.common.QueueFeeder;
+import net.sf.jukebox.datastream.signal.model.DataSample;
+
+public class JsonRendererTest {
     
-    public void testNothing() {
-        
-        // This is here to make JUnit happy until there are real tests 
+    private final Logger logger = Logger.getLogger(getClass());
+
+    @Test
+    public void testRender0() {
+
+        NDC.push("testRender");
+
+        try {
+
+            ThermostatModel t = new ThermostatModel("json-thermostat", new NullSensor("null-address", 100), 20, 1, 0.000002,0, 2);
+            Map<String, Object> context = new HashMap<String, Object>();
+            BlockingQueue<ZoneSnapshot> queue = new LinkedBlockingQueue<ZoneSnapshot>();
+            
+            context.put(QueueFeeder.QUEUE_KEY, queue);
+            
+            ThermostatRenderer tr = new ThermostatRenderer(t, context, null);
+
+            DataSample<Double> demand = new DataSample<Double>("source-d", "signature-d", new Double(0), null);
+            ThermostatSignal ts = new ThermostatSignal(true, true, true, true, demand);
+            
+            tr.consume(new DataSample<ThermostatSignal>("source-ts", "signature-ts", ts, null));
+            
+            assertEquals("wrong queue size", 1, queue.size());
+            
+            logger.debug("queue head: " + queue.peek());
+            
+            assertEquals("wrong toString()", "json-thermostat: Cooling, CALLING, signal=0.0, current=0.0, setpoint=20.0, on hold", queue.peek().toString());
+
+        } finally {
+            NDC.pop();
+        }
     }
 }
