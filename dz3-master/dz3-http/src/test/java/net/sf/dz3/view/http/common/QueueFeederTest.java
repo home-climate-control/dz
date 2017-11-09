9f1391fc097395566e48746a541e03c21e7eff91
Vadim Tkachenko <vt@seriousinsights.com>
2017-11-09 02:30:48 -0700

    Removed trailing spaces

--- dz3-master/dz3-http/src/test/java/net/sf/dz3/view/http/common/QueueFeederTest.java
+++ dz3-master/dz3-http/src/test/java/net/sf/dz3/view/http/common/QueueFeederTest.java
@@ -8,29 +8,29 @@ import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 public class QueueFeederTest {
-    
+
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
     @Test
     public void testNullContext() {
-        
+
         Map<String, Object> context = null;
-        
+
         thrown.expect(IllegalArgumentException.class);
         thrown.expectMessage("null context, doesn't make sense");
-        
+
         QueueFeeder<String> qf = new QueueFeeder<String>(context) {};
     }
 
     @Test
     public void testNullQueue() {
-        
+
         Map<String, Object> context = new HashMap<String, Object>();
-        
+
         thrown.expect(IllegalArgumentException.class);
         thrown.expectMessage("null queue, doesn't make sense");
-        
+
         QueueFeeder<String> qf = new QueueFeeder<String>(context) {};
     }
 }
--- dz3-master/dz3-http/src/test/java/net/sf/dz3/view/http/v2/JsonRendererTest.java
+++ dz3-master/dz3-http/src/test/java/net/sf/dz3/view/http/v2/JsonRendererTest.java
@@ -18,7 +18,7 @@ import net.sf.dz3.view.http.common.QueueFeeder;
 import net.sf.jukebox.datastream.signal.model.DataSample;
 
 public class JsonRendererTest {
-    
+
     private final Logger logger = Logger.getLogger(getClass());
 
     @Test
@@ -31,20 +31,20 @@ public class JsonRendererTest {
             ThermostatModel t = new ThermostatModel("json-thermostat", new NullSensor("null-address", 100), 20, 1, 0.000002,0, 2);
             Map<String, Object> context = new HashMap<String, Object>();
             BlockingQueue<ZoneSnapshot> queue = new LinkedBlockingQueue<ZoneSnapshot>();
-            
+
             context.put(QueueFeeder.QUEUE_KEY, queue);
-            
+
             ThermostatRenderer tr = new ThermostatRenderer(t, context, null);
 
             DataSample<Double> demand = new DataSample<Double>("source-d", "signature-d", new Double(0), null);
             ThermostatSignal ts = new ThermostatSignal(true, true, true, true, demand);
-            
+
             tr.consume(new DataSample<ThermostatSignal>("source-ts", "signature-ts", ts, null));
-            
+
             assertEquals("wrong queue size", 1, queue.size());
-            
+
             logger.debug("queue head: " + queue.peek());
-            
+
             assertEquals("wrong toString()", "json-thermostat: Cooling, CALLING, signal=0.0, current=0.0, setpoint=20.0, on hold", queue.peek().toString());
 
         } finally {
