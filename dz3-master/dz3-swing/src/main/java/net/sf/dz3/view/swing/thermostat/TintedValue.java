275c0aeb6839ac6d2b0d2d6e78f105c58c4c7738
Vadim Tkachenko <vt@homeclimatecontrol.com>
2018-03-01 21:17:39 -0700

    Documentation update

--- dz3-master/dz3-swing/src/main/java/net/sf/dz3/view/swing/thermostat/TintedValue.java
+++ dz3-master/dz3-swing/src/main/java/net/sf/dz3/view/swing/thermostat/TintedValue.java
@@ -1,14 +1,28 @@
 package net.sf.dz3.view.swing.thermostat;
 
 /**
- * Intended to hold a data sample for the {@link Chart variable color chart}.
+ * Intended to hold a data sample for the {@link AbstractChart variable color chart}.
  * 
- * @author Copyright &copy; <a href="mailto:vt@freehold.crocodile.org">Vadim Tkachenko</a> 2001-2016
+ * @author Copyright &copy; <a href="mailto:vt@freehold.crocodile.org">Vadim Tkachenko</a> 2001-2018
  */
 public class TintedValue {
 
+    /**
+     * The Y coordinate on the chart.
+     */
     public final double value;
+
+    /**
+     * The tint.
+     *
+     * Allowable values are {@code -1d} to {@code 1d}. Translates into the point color on the chart,
+     * {@code -1d} and {@code 1d} being the far extremes of the color gradient.
+     */
     public final double tint;
+
+    /**
+     * {@code true} if this particular point on the chart needs to be emphasized.
+     */
     public final boolean emphasize;
 
     public TintedValue(double value, double tint, boolean emphasize) {
