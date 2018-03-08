633358ff52c1fe29a2c0aad0e176ee065f76f3e0
Vadim Tkachenko <vt@homeclimatecontrol.com>
2018-03-07 20:07:50 -0700

    Moved empty lines to correct places

--- dz3-master/dz3-swing/src/main/java/net/sf/dz3/view/swing/thermostat/TintedValue.java
+++ dz3-master/dz3-swing/src/main/java/net/sf/dz3/view/swing/thermostat/TintedValue.java
@@ -10,16 +10,16 @@ public class TintedValue {
     /**
      * Value Y coordinate on the chart.
      */
-
     public final double value;
+
     /**
      * The tint.
      *
      * Allowable values are {@code -1d} to {@code 1d}. Translates into the point color on the chart,
      * {@code -1d} and {@code 1d} being the far extremes of the color gradient.
      */
-
     public final double tint;
+
     /**
      * {@code true} if this particular point on the chart needs to be emphasized.
      */
