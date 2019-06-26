93de6c24b853df1b27b3edeabc32aafb3f0e4e90
Vadim Tkachenko <vt@homeclimatecontrol.com>
2019-06-25 23:30:56 -0700

    Implemented a rudimentary InfluxDB logger (#93)

--- dz3-master/dz3-influxdb/src/main/java/net/sf/dz3/view/influxdb/v1/InfluxDbLogger.java
+++ dz3-master/dz3-influxdb/src/main/java/net/sf/dz3/view/influxdb/v1/InfluxDbLogger.java
@@ -2,6 +2,13 @@ package net.sf.dz3.view.influxdb.v1;
 
 import java.io.IOException;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.logging.log4j.ThreadContext;
+import org.influxdb.InfluxDB;
+import org.influxdb.InfluxDBFactory;
+import org.influxdb.dto.Point;
+import org.influxdb.dto.Query;
 
 import net.sf.jukebox.datastream.logger.impl.AbstractLogger;
 import net.sf.jukebox.datastream.signal.model.DataSample;
@@ -15,29 +22,101 @@ import net.sf.jukebox.datastream.signal.model.DataSource;
  *
  * @author Copyright &copy; <a href="mailto:vt@freehold.crocodile.org">Vadim Tkachenko</a> 2001-2019
  */
-public class InfluxDbLogger extends AbstractLogger<Number> {
+public class InfluxDbLogger<E extends Number> extends AbstractLogger<E> {
 
-    public InfluxDbLogger(Set<DataSource<Number>> producers) {
+    private final String dbName = "dz";
+    private final String instance;
+    private final String dbURL;
+    private final String username;
+    private final String password;
+
+    private InfluxDB db;
+
+    public InfluxDbLogger(
+            Set<DataSource<E>> producers,
+            String instance,
+            String dbURL,
+            String username,
+            String password) {
         super(producers);
+
+        this.instance = instance;
+        this.dbURL = dbURL;
+        this.username = username;
+        this.password = password;
     }
 
     @Override
-    protected void consume(String signature, DataSample<Number> value) {
-        throw new IllegalStateException("Not Implemented");
+    protected void consume(String signature, DataSample<E> value) {
+        ThreadContext.push("consume");
+
+        try {
+
+            db.write(Point.measurement(value.sourceName)
+                    .time(value.timestamp, TimeUnit.MILLISECONDS)
+                    .addField("signature", value.signature)
+                    .addField("instance", instance)
+                    .addField("sample", value.sample)
+                    .build());
+
+        } finally {
+            ThreadContext.pop();
+        }
     }
 
     @Override
-    protected void createChannel(String arg0, String arg1, long arg2) throws IOException {
-        throw new IllegalStateException("Not Implemented");
+    protected void createChannel(String name, String signature, long timestamp) throws IOException {
+
+        ThreadContext.push("createChannel");
+
+        try {
+
+            db.query(new Query("CREATE DATABASE " + dbName));
+            db.setDatabase(dbName);
+
+        } finally {
+            ThreadContext.pop();
+        }
+    }
+
+    private String doubleQuote(String source) {
+        return "\"" + source + "\"";
     }
 
     @Override
     protected void startup() throws Throwable {
-        throw new IllegalStateException("Not Implemented");
+
+        ThreadContext.push("startup");
+
+        try {
+
+            if (username == null || "".equals(username) || password == null || "".equals(password)) {
+                logger.warn("one of (username, password) is null or missing, connecting unauthenticated - THIS IS A BAD IDEA");
+                logger.warn("see https://docs.influxdata.com/influxdb/v1.7/administration/authentication_and_authorization/");
+                logger.warn("(username, password) = (" + username + ", " + password + ")");
+
+                db = InfluxDBFactory.connect(dbURL);
+
+            } else {
+                db = InfluxDBFactory.connect(dbURL, username, password);
+            }
+
+        } finally {
+            ThreadContext.pop();
+        }
     }
 
     @Override
     protected void shutdown() throws Throwable {
-        throw new IllegalStateException("Not Implemented");
+
+        ThreadContext.push("shutdown");
+
+        try {
+
+            db.close();
+
+        } finally {
+            ThreadContext.pop();
+        }
     }
 }
