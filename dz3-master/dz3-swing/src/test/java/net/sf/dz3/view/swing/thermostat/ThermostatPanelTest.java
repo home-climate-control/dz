bc0740f7345ebb647e2fd07fb819124edef8b7a5
Vadim Tkachenko <vt@homeclimatecontrol.com>
2020-05-14 20:48:15 -0700

    Oops... fixed the test (#136)

--- dz3-master/dz3-swing/src/test/java/net/sf/dz3/view/swing/thermostat/ThermostatPanelTest.java
+++ dz3-master/dz3-swing/src/test/java/net/sf/dz3/view/swing/thermostat/ThermostatPanelTest.java
@@ -8,6 +8,7 @@ import net.sf.dz3.controller.pid.SimplePidController;
 import net.sf.dz3.device.model.impl.ThermostatModel;
 import net.sf.dz3.device.sensor.AnalogSensor;
 import net.sf.dz3.view.swing.ScreenDescriptor;
+import net.sf.dz3.view.swing.TemperatureUnit;
 import net.sf.jukebox.datastream.signal.model.DataSample;
 import net.sf.jukebox.datastream.signal.model.DataSink;
 import net.sf.jukebox.jmx.JmxDescriptor;
@@ -17,7 +18,7 @@ public class ThermostatPanelTest extends TestCase {
     public void testNullScheduler() {
 
         ThermostatModel tm = new ThermostatModel("model", new MockSensor(), new SimplePidController(1, 1, 1, 1, 1));
-        ThermostatPanel tp = new ThermostatPanel(tm, new ScreenDescriptor("", new Dimension(0, 0), null, null, null), null);
+        ThermostatPanel tp = new ThermostatPanel(tm, new ScreenDescriptor("", new Dimension(0, 0), null, null, null), null, TemperatureUnit.CELSIUS);
 
         tp.keyPressed(new KeyEvent(tp, 0, 0, 0, 0, 's', 0));
     }
