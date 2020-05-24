1f6feab020146ab84eeb0acb97003764f13134db
Vadim Tkachenko <vt@homeclimatecontrol.com>
2020-05-24 01:50:40 -0700

    Now using the new chart (#142)

--- dz3-master/dz3-swing/src/main/java/net/sf/dz3/view/swing/thermostat/ThermostatPanel.java
+++ dz3-master/dz3-swing/src/main/java/net/sf/dz3/view/swing/thermostat/ThermostatPanel.java
@@ -86,7 +86,7 @@ public class ThermostatPanel extends JPanel implements KeyListener {
     private final JLabel periodLabel = new JLabel("", SwingConstants.LEFT);
 
     // 3 hours
-    private final AbstractChart chart = new Chart2016(Clock.systemUTC(), 1000 * 60 * 60 * 3);
+    private final AbstractChart chart = new Chart2020(Clock.systemUTC(), 1000L * 60 * 60 * 3);
 
     private static final String NO_PERIOD = "(no period is active)";
 
