a30dc1fa6bd4dffc1d7fce692ce00cf4c09ea4a2
Vadim Tkachenko <vt@homeclimatecontrol.com>
2021-06-11 21:24:39 -0700

    Implemented UnitSignalSplitter as a reactive entity, refactored HvacSignalSplitter (#183)

--- dz3-model/src/main/java/net/sf/dz4/signal/HvacSignalSplitter.java
+++ dz3-model/src/main/java/net/sf/dz4/signal/HvacSignalSplitter.java
@@ -13,8 +13,9 @@ import reactor.core.publisher.Flux;
  *
  * @author Copyright &copy; <a href="mailto:vt@homeclimatecontrol.com">Vadim Tkachenko</a> 2009-2021
  */
-public class HvacSignalSplitter {
+public class HvacSignalSplitter extends UnitSignalSplitter<HvacSignal> {
 
+    @Override
     public Flux<DataSample<?>> split(Flux<DataSample<HvacSignal>> source) {
         return source.flatMap(s -> Flux.just(getMode(s), getRunning(s), getDemand(s), getUptime(s)));
     }
@@ -25,33 +26,6 @@ public class HvacSignalSplitter {
         return new DataSample<>(
                 signal.timestamp, sourceName, signature,
                 signal.isError() ? null : signal.sample.mode,
-                signal.isError() ? signal.error : null);
-    }
-
-    private DataSample<Boolean> getRunning(DataSample<HvacSignal> signal) {
-        var sourceName = signal.sourceName + ".running";
-        var signature = MessageDigestCache.getMD5(sourceName).substring(0, 19);
-        return new DataSample<>(
-                signal.timestamp, sourceName, signature,
-                signal.isError() ? null : signal.sample.running,
-                signal.isError() ? signal.error : null);
-    }
-
-    private DataSample<Double> getDemand(DataSample<HvacSignal> signal) {
-        var sourceName = signal.sourceName + ".demand";
-        var signature = MessageDigestCache.getMD5(sourceName).substring(0, 19);
-        return new DataSample<>(
-                signal.timestamp, sourceName, signature,
-                signal.isError() ? null : signal.sample.demand,
-                signal.isError() ? signal.error : null);
-    }
-
-    private DataSample<Long> getUptime(DataSample<HvacSignal> signal) {
-        var sourceName = signal.sourceName + ".uptime";
-        var signature = MessageDigestCache.getMD5(sourceName).substring(0, 19);
-        return new DataSample<>(
-                signal.timestamp, sourceName, signature,
-                signal.isError() ? null : signal.sample.uptime,
                 signal.isError() ? signal.error : null);
     }
 }
---
+++ dz3-model/src/main/java/net/sf/dz4/signal/UnitSignalSplitter.java
@@ -0,0 +1,47 @@
+package net.sf.dz4.signal;
+
+import com.homeclimatecontrol.jukebox.datastream.signal.model.DataSample;
+import net.sf.dz3.device.model.UnitSignal;
+import net.sf.dz3.util.digest.MessageDigestCache;
+import reactor.core.publisher.Flux;
+
+/**
+ * Consumes a complex {@link UnitSignal}, emits several
+ * {@link DataSample simpler signals}
+ * suitable for consumption by non-specialized metrics loggers.
+ *
+ * @author Copyright &copy; <a href="mailto:vt@homeclimatecontrol.com">Vadim Tkachenko</a> 2009-2021
+ */
+public class UnitSignalSplitter<T extends UnitSignal> {
+
+    public Flux<DataSample<?>> split(Flux<DataSample<T>> source) {
+        return source.flatMap(s -> Flux.just(getRunning(s), getDemand(s), getUptime(s)));
+    }
+
+    protected DataSample<Boolean> getRunning(DataSample<T> signal) {
+        var sourceName = signal.sourceName + ".running";
+        var signature = MessageDigestCache.getMD5(sourceName).substring(0, 19);
+        return new DataSample<>(
+                signal.timestamp, sourceName, signature,
+                signal.isError() ? null : signal.sample.running,
+                signal.isError() ? signal.error : null);
+    }
+
+    protected DataSample<Double> getDemand(DataSample<T> signal) {
+        var sourceName = signal.sourceName + ".demand";
+        var signature = MessageDigestCache.getMD5(sourceName).substring(0, 19);
+        return new DataSample<>(
+                signal.timestamp, sourceName, signature,
+                signal.isError() ? null : signal.sample.demand,
+                signal.isError() ? signal.error : null);
+    }
+
+    protected DataSample<Long> getUptime(DataSample<T> signal) {
+        var sourceName = signal.sourceName + ".uptime";
+        var signature = MessageDigestCache.getMD5(sourceName).substring(0, 19);
+        return new DataSample<>(
+                signal.timestamp, sourceName, signature,
+                signal.isError() ? null : signal.sample.uptime,
+                signal.isError() ? signal.error : null);
+    }
+}
