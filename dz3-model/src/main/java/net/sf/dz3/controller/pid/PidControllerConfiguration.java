7ee79d81db487f73201235e81a7931f603cd285f
Vadim Tkachenko <vt@homeclimatecontrol.com>
2021-07-04 16:41:25 -0700

    Updated signatures to conform to JmxAttribute (#189)

--- dz3-model/src/main/java/net/sf/dz3/controller/pid/PidControllerConfiguration.java
+++ dz3-model/src/main/java/net/sf/dz3/controller/pid/PidControllerConfiguration.java
@@ -1,9 +1,14 @@
 package net.sf.dz3.controller.pid;
 
+import com.homeclimatecontrol.jukebox.jmx.JmxAttribute;
+
 /**
  * @author <a href="mailto:vt@homeclimatecontrol.com">Vadim Tkachenko</a> 2005-2021
  */
 public interface PidControllerConfiguration extends AbstractPidControllerConfiguration {
+
+  @JmxAttribute(description = "Integral component time span")
+  long getIspan();
 
   /**
    * Set the controller integral span.
@@ -11,6 +16,9 @@ public interface PidControllerConfiguration extends AbstractPidControllerConfigu
    * @param iSpan Integral span, in milliseconds.
    */
   void setIspan(long iSpan);
+
+  @JmxAttribute(description = "Derivative component time span")
+  long getDspan();
 
   /**
    * Set the controller derivative span.
