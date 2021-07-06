45c614d6d1806ead837cf1ed85e3e182cb474be6
Vadim Tkachenko <vt@homeclimatecontrol.com>
2021-07-05 22:59:56 -0700

    Checkpoint: rendering the sensor panel (#186)

--- dz3-swing/src/main/java/net/sf/dz3/view/swing/sensor/SensorChart.java
+++ dz3-swing/src/main/java/net/sf/dz3/view/swing/sensor/SensorChart.java
@@ -4,10 +4,14 @@ import com.homeclimatecontrol.jukebox.datastream.signal.model.DataSample;
 import com.homeclimatecontrol.jukebox.util.Interval;
 import net.sf.dz3.controller.DataSet;
 import net.sf.dz3.view.swing.AbstractChart;
+import net.sf.dz3.view.swing.ColorScheme;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics2D;
 import java.awt.Insets;
+import java.awt.RenderingHints;
+import java.awt.geom.Line2D;
 import java.time.Clock;
 
 public class SensorChart extends AbstractChart<Double> {
@@ -76,7 +80,7 @@ public class SensorChart extends AbstractChart<Double> {
 
     @Override
     protected boolean isDataAvailable() {
-        return false;
+        return dataMax != null && dataMin != null;
     }
 
     @Override
@@ -85,8 +89,73 @@ public class SensorChart extends AbstractChart<Double> {
     }
 
     @Override
-    protected void paintCharts(Graphics2D g2d, Dimension boundary, Insets insets, long now, double xScale, long xOffset, double yScale, double yOffset) {
+    protected void paintCharts(Graphics2D g2d, Dimension boundary, Insets insets,
+                               long now, double xScale, long xOffset, double yScale, double yOffset) {
 
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        paintValues(g2d, insets, now, xScale, xOffset, yScale, yOffset, values);
+    }
+
+    private void paintValues(Graphics2D g2d, Insets insets,
+                             long now, double xScale, long xOffset, double yScale, double yOffset,
+                             DataSet<Double> ds) {
+        Long timeTrailer = null;
+        Double trailer = null;
+
+        for (var di = ds.entryIterator(); di.hasNext();) {
+
+            var entry = di.next();
+            var timeNow = entry.getKey();
+            var cursor = entry.getValue();
+
+            if (timeTrailer != null) {
+
+                var x0 = (timeTrailer - xOffset) * xScale + insets.left;
+                var y0 = (yOffset - trailer) * yScale + insets.top;
+
+                var x1 = (timeNow - xOffset) * xScale + insets.left;
+                var y1 = (yOffset - cursor) * yScale + insets.top;
+
+                // Decide whether the line is alive or dead
+
+                if (timeNow - timeTrailer > DEAD_TIMEOUT) {
+
+                    // It's dead, all right
+                    // Paint the horizontal line in dead color and skew the x0 so the next part will be painted vertical
+                    var startColor = ColorScheme.offMap.sensorStale;
+
+                    // End color differs from the start in alpha, not hue - this plays nicer with backgrounds
+                    // Even though this is a memory allocation, it won't affect performance since [hopefully]
+                    // there'll be just a few dead drops
+                    var endColor = new Color(startColor.getRed(), startColor.getGreen(), startColor.getBlue(), 64);
+
+                    drawGradientLine(g2d, x0, y0, x1, y0, startColor, endColor, false);
+
+                    x0 = x1;
+                }
+
+                g2d.setPaint(ColorScheme.offMap.sensorNormal);
+                Line2D line = new Line2D.Double(x0, y0, x1, y1);
+                g2d.draw(line);
+            }
+
+            timeTrailer = timeNow;
+            trailer = cursor;
+        }
+
+        if (timeTrailer != null && now - timeTrailer > DEAD_TIMEOUT) {
+
+            // There's a gap on the right, let's fill it
+
+            var x0 = (timeTrailer - xOffset) * xScale + insets.left;
+            var x1 = (now - xOffset) * xScale + insets.left;
+            var y = (yOffset - trailer) * yScale + insets.top;
+
+            var startColor = ColorScheme.offMap.sensorStale;
+            var endColor = getBackground();
+
+            drawGradientLine(g2d, x0, y, x1, y, startColor, endColor, false);
+        }
     }
 
     /**
