2a5a859c2042d485e526c2bc77779267abfb88a3
Vadim Tkachenko <vt@homeclimatecontrol.com>
2021-09-19 21:09:51 -0700

    Lowered the log level of a spammy message

--- dz3-common/src/main/java/net/sf/dz3/util/counter/TimeBasedUsage.java
+++ dz3-common/src/main/java/net/sf/dz3/util/counter/TimeBasedUsage.java
@@ -5,48 +5,45 @@ import org.apache.logging.log4j.Logger;
 
 /**
  * Simple time based usage counter.
- * 
- * @author Copyright &copy; <a href="mailto:vt@homeclimatecontrol.com">Vadim Tkachenko</a> 2001-2018
+ *
+ * @author Copyright &copy; <a href="mailto:vt@homeclimatecontrol.com">Vadim Tkachenko</a> 2001-2021
  */
 public class TimeBasedUsage implements CounterStrategy {
-    
+
     private final Logger logger = LogManager.getLogger(getClass());
-    
+
     private long last;
     private boolean running = false;
-    
+
     public TimeBasedUsage() {
-    
+
         this(System.currentTimeMillis());
     }
 
     public TimeBasedUsage(long timestamp) {
-    
+
         this.last = timestamp;
     }
 
     /**
      * Interpret {@code value} as "running or not" and use {@code timestamp}
      * to calculate usage.
-     * 
+     *
      * @return milliseconds consumed.
      */
     @Override
     public synchronized long consume(long timestamp, double value) {
-        
+
         if (timestamp < last) {
-            
-            logger.warn("Can't go back in time - timestamp ("
-                    + timestamp + " is " + (last - timestamp) + "ms less than last known (" + last + "), sample ignored");
-            
+            logger.debug("Can't go back in time - timestamp ({} is {}ms less than last known ({}), sample ignored", timestamp, last - timestamp, last);
             return 0;
         }
 
         long result = running ? timestamp - last : 0;
-        
+
         last = timestamp;
         running = value > 0;
-        
+
         return result;
     }
 }
