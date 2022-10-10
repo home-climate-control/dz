b2af0e0070a06015aed8da89743818dc7ea398ad
Vadim Tkachenko <vt@homeclimatecontrol.com>
2022-10-09 21:54:07 -0700

    Refactored to support alternative implementations at the same time (#243)

---
+++ dz3r-model/src/main/java/net/sf/dz3r/device/actuator/economizer/AbstractEconomizer.java
@@ -0,0 +1,263 @@
+package net.sf.dz3r.device.actuator.economizer;
+
+import net.sf.dz3r.device.Addressable;
+import net.sf.dz3r.device.actuator.Switch;
+import net.sf.dz3r.model.HvacMode;
+import net.sf.dz3r.model.Zone;
+import net.sf.dz3r.signal.Signal;
+import net.sf.dz3r.signal.SignalProcessor;
+import net.sf.dz3r.signal.hvac.ThermostatStatus;
+import net.sf.dz3r.signal.hvac.ZoneStatus;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.ThreadContext;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
+import reactor.core.scheduler.Schedulers;
+
+public abstract class AbstractEconomizer <A extends Comparable<A>> implements SignalProcessor<Double, ZoneStatus, String>, Addressable<String> {
+
+    protected final Logger logger = LogManager.getLogger();
+
+    public final String name;
+    public final EconomizerConfig config;
+
+    public final Zone targetZone;
+
+    protected final Switch<A> targetDevice;
+
+    /**
+     * Last known indoor temperature. Can't be an error.
+     */
+    private Signal<Double, String> indoor;
+
+    /**
+     * Last known ambient temperature. Can't be an error.
+     */
+    private Signal<Double, Void> ambient;
+
+    /**
+     * Mirrors the state of {@link #targetDevice} to avoid expensive operations.
+     */
+    private Boolean actuatorState;
+    private FluxSink<Pair<Signal<Double, String>, Signal<Double, Void>>> combinedSink;
+
+    /**
+     * Create an instance.
+     *
+     * Note that only the {@code ambientFlux} argument is present, indoor flux is provided to {@link #compute(Flux)}.
+     *
+     * @param name Human readable name.
+     * @param targetZone Zone to serve.
+     * @param ambientFlux Flux from the ambient temperature sensor.
+     * @param targetDevice Switch to control the economizer actuator.
+     */
+    protected AbstractEconomizer(
+            String name,
+            EconomizerConfig config,
+            Zone targetZone,
+            Flux<Signal<Double, Void>> ambientFlux,
+            Switch<A> targetDevice) {
+        this.name = name;
+        this.config = config;
+        this.targetZone = targetZone;
+        this.targetDevice = targetDevice;
+
+        Flux
+                .create(this::connectCombined)
+                .subscribeOn(Schedulers.boundedElastic())
+                .doOnNext(pair -> logger.debug("raw indoor={}, ambient={}", pair.getLeft(), pair.getRight()))
+                .filter(pair -> pair.getLeft() != null && pair.getRight() != null)
+                .filter(pair -> !pair.getLeft().isError() && !pair.getRight().isError())
+                .map(this::computeCombined)
+                .flatMap(this::computeDeviceState)
+                .doOnNext(this::recordDeviceState)
+
+                // Let the transmission layer figure out the dupes, they have a better idea about what to do with them
+                .flatMap(state -> targetDevice.setState(actuatorState))
+
+                // VT: NOTE: Careful when testing, this will consume everything thrown at it immediately
+                .subscribe();
+
+        ambientFlux
+                .subscribeOn(Schedulers.boundedElastic())
+                .doOnNext(this::recordAmbient)
+
+                // VT: NOTE: Careful when testing, this will consume everything thrown at it immediately
+                .subscribe();
+    }
+
+    @Override
+    public String getAddress() {
+        return name;
+    }
+    private void recordAmbient(Signal<Double, Void> ambient) {
+
+        this.ambient = ambient;
+        combinedSink.next(new ImmutablePair<>(indoor, ambient));
+    }
+
+    private void recordIndoor(Signal<Double, String> indoor) {
+
+        this.indoor = indoor;
+        combinedSink.next(new ImmutablePair<>(indoor, ambient));
+    }
+
+    private void connectCombined(FluxSink<Pair<Signal<Double, String>, Signal<Double, Void>>> sink) {
+        combinedSink = sink;
+    }
+
+    private void recordDeviceState(Signal<Boolean, Void> stateSignal) {
+
+        var newState = stateSignal.getValue();
+
+        if ((actuatorState == null && newState != null) || newState.compareTo(actuatorState) != 0) {
+
+            logger.info("{}: state change: {} => {}", name, actuatorState, newState);
+            actuatorState = newState;
+        }
+    }
+
+    /**
+     * Compute the virtual zone signal.
+     *
+     * @param indoorFlux Indoor temperature flux.
+     *
+     * @return Flux to send to {@code UnitDirector} instead of the zone flux.
+     */
+    @Override
+    public Flux<Signal<ZoneStatus, String>> compute(Flux<Signal<Double, String>> indoorFlux) {
+
+        // Summary of what we need to do here:
+
+        // - compare the indoor flux against the changeover and target temperature, and
+        //   issue a control signal when the actuator needs to be on
+        // - feed the indoor flux to the zone, intercept the zone output, and suppress demand there
+        //   when the actuator is on
+
+        indoorFlux
+                .subscribeOn(Schedulers.boundedElastic())
+                .doOnNext(this::recordIndoor)
+
+                // VT: NOTE: Careful when testing, this will consume everything thrown at it immediately
+                .subscribe();
+
+        return targetZone
+                .compute(indoorFlux)
+                .map(this::computeZone);
+    }
+
+    private Signal<Double, Void> computeCombined(Pair<Signal<Double, String>, Signal<Double, Void>> pair) {
+
+        ThreadContext.push("computeCombined");
+
+        try {
+
+            // Null and error values have already been filtered out
+            var indoorTemperature = pair.getLeft().getValue();
+            var ambientTemperature = pair.getRight().getValue();
+
+            logger.debug("valid indoor={}, ambient={}", indoorTemperature, ambientTemperature);
+
+            // Adjusted for mode
+            var ambientDelta = getAmbientDelta(indoorTemperature, ambientTemperature);
+            var targetDelta = getTargetDelta(indoorTemperature);
+
+            // Negative target data indicates indoor temperature beyond threshold; ambient is irrelevant at this point,
+            // economizer needs to be shut off
+            var signal = targetDelta > 0
+                    ? ambientDelta
+                    : targetDelta;
+
+            logger.debug("ambientD={}, targetD={}, signal={}", ambientDelta, targetDelta, signal);
+
+            // The latter one wins
+            var timestamp = indoor.timestamp.isAfter(ambient.timestamp) ? indoor.timestamp : ambient.timestamp;
+
+            return new Signal<>(timestamp, signal);
+
+        } finally {
+            ThreadContext.pop();
+        }
+    }
+
+    /**
+     * Get the {@link EconomizerConfig#changeoverDelta ambient} delta signal.
+     *
+     * @return Positive value indicates demand, negative indicates lack thereof, regardless of mode.
+     */
+    double getAmbientDelta(double indoor, double ambient) {
+
+        return config.mode == HvacMode.COOLING
+                ? indoor - (ambient + config.changeoverDelta)
+                : ambient - (indoor + config.changeoverDelta);
+    }
+
+    /**
+     * Get the {@link EconomizerConfig#targetTemperature} delta signal.
+     *
+     * @return Positive value indicates demand, negative indicates lack thereof, regardless of mode.
+     */
+    double getTargetDelta(double indoor) {
+        return config.mode == HvacMode.COOLING
+                ? indoor - config.targetTemperature
+                : config.targetTemperature - indoor;
+    }
+
+    private Flux<Signal<Boolean, Void>> computeDeviceState(Signal<Double, Void> signal) {
+
+        ThreadContext.push("computeDeviceState");
+
+        try {
+
+            // VT: FIXME: This will create LOTS of jitter, don't use with units that can't deal with it
+
+            Boolean state = signal.getValue() > 0 ? Boolean.TRUE : Boolean.FALSE;
+
+            logger.debug("state={}", state);
+
+            return Flux.just(new Signal<>(signal.timestamp, state));
+
+        } finally {
+            ThreadContext.pop();
+        }
+    }
+
+    private Signal<ZoneStatus, String> computeZone(Signal<ZoneStatus, String> source) {
+
+        if (actuatorState == null || actuatorState == Boolean.FALSE) {
+
+            // Economizer inactive, no change required
+            return source;
+        }
+
+        if (config.keepHvacOn) {
+
+            // We're feeding indoor air to HVAC air return, right?
+            return source;
+        }
+
+        if (source.isError()) {
+
+            // How did it get here? This log message is likely a dupe, need to keep an eye on it
+            logger.warn("error signal in economizer pipeline? {}", source);
+
+            // Can't do anything meaningful with it here
+            return source;
+        }
+
+        // Need to suppress demand and keep the HVAC off while the economizer is on
+        var adjusted = new ZoneStatus(
+                source.getValue().settings,
+                new ThermostatStatus(0, false));
+
+        return new Signal<>(
+                source.timestamp,
+                adjusted,
+                source.payload,
+                source.status,
+                source.error);
+    }
+}
--- dz3r-model/src/main/java/net/sf/dz3r/device/actuator/economizer/v1/Economizer.java
+++ dz3r-model/src/main/java/net/sf/dz3r/device/actuator/economizer/v1/Economizer.java
@@ -1,18 +1,11 @@
 package net.sf.dz3r.device.actuator.economizer.v1;
 
-import net.sf.dz3r.device.Addressable;
 import net.sf.dz3r.device.actuator.Switch;
+import net.sf.dz3r.device.actuator.economizer.AbstractEconomizer;
 import net.sf.dz3r.device.actuator.economizer.EconomizerConfig;
 import net.sf.dz3r.model.Zone;
 import net.sf.dz3r.signal.Signal;
-import net.sf.dz3r.signal.SignalProcessor;
-import net.sf.dz3r.signal.hvac.ThermostatStatus;
-import net.sf.dz3r.signal.hvac.ZoneStatus;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.ThreadContext;
 import reactor.core.publisher.Flux;
-import reactor.core.scheduler.Schedulers;
 
 /**
  * Economizer implementation.
@@ -21,31 +14,8 @@ import reactor.core.scheduler.Schedulers;
  *
  * @param <A> Actuator device address type.
  */
-public class Economizer<A extends Comparable<A>> implements SignalProcessor<Double, ZoneStatus, String>, Addressable<String> {
+public class Economizer<A extends Comparable<A>> extends AbstractEconomizer<A> {
 
-    private final Logger logger = LogManager.getLogger();
-
-    public final String name;
-    public final EconomizerConfig config;
-
-    public final Zone targetZone;
-
-    /**
-     * Last known indoor temperature. Can't be an error.
-     */
-    private Signal<Double, String> indoor;
-
-    /**
-     * Last known ambient temperature. Can't be an error.
-     */
-    private Signal<Double, Void> ambient;
-
-    private final Switch<A> targetDevice;
-
-    /**
-     * Mirrors the state of {@link #targetDevice} to avoid expensive operations.
-     */
-    private Boolean actuatorState;
 
     /**
      * Create an instance.
@@ -64,223 +34,6 @@ public class Economizer<A extends Comparable<A>> implements SignalProcessor<Doub
             Flux<Signal<Double, Void>> ambientFlux,
             Switch<A> targetDevice) {
 
-        this.name = name;
-        this.config = config;
-        this.targetZone = targetZone;
-        this.targetDevice = targetDevice;
-
-        ambientFlux
-                .subscribeOn(Schedulers.boundedElastic())
-                .flatMap(this::computeAmbient)
-                .doOnNext(this::recordDeviceState)
-
-                // Let the transmission layer figure out the dupes, they have a better idea about what to do with them
-                .flatMap(state -> targetDevice.setState(actuatorState))
-
-                // VT: NOTE: Careful when testing, this will consume everything thrown at it immediately
-                .subscribe();
-    }
-
-    private void recordDeviceState(Signal<Boolean, Void> stateSignal) {
-
-        var newState = stateSignal.getValue();
-
-        if ((actuatorState == null && newState != null) || newState.compareTo(actuatorState) != 0) {
-
-            logger.info("{}: state change: {} => {}", name, actuatorState, newState);
-            actuatorState = newState;
-        }
-    }
-
-    @Override
-    public String getAddress() {
-        return name;
-    }
-
-    /**
-     * Compute the virtual zone signal.
-     *
-     * @param indoorFlux Indoor temperature flux.
-     *
-     * @return Flux to send to {@code UnitDirector} instead of the zone flux.
-     */
-    @Override
-    public Flux<Signal<ZoneStatus, String>> compute(Flux<Signal<Double, String>> indoorFlux) {
-
-        // Summary of what we need to do here:
-
-        // - compare the indoor flux against the changeover and target temperature, and
-        //   issue a control signal when the actuator needs to be on
-        // - feed the indoor flux to the zone, intercept the zone output, and suppress demand there
-        //   when the actuator is on
-
-        indoorFlux
-                .subscribeOn(Schedulers.boundedElastic())
-                .flatMap(this::computeIndoor)
-                .doOnNext(this::recordDeviceState)
-
-                // Let the transmission layer figure out the dupes, they have a better idea about what to do with them
-                .flatMap(state -> targetDevice.setState(actuatorState))
-
-                // VT: NOTE: Careful when testing, this will consume everything thrown at it immediately
-                .subscribe();
-
-        return targetZone
-                .compute(indoorFlux)
-                .map(this::computeZone);
-    }
-
-    /**
-     * Aggregate with {@link #ambient} and pass control to {@link #computeDeviceState(Signal, Signal)}.
-     *
-     * @return Empty flux if it isn't possible to compute the value, or the target device to execute.
-     */
-    private Flux<Signal<Boolean, Void>> computeIndoor(Signal<Double, String> indoor) {
-
-        ThreadContext.push("computeIndoor");
-
-        try {
-
-            logger.debug("indoor={}", indoor);
-
-            if (indoor.isError()) {
-
-                // Can't do much here other than shut the economizer off, who knows what's going on there
-                return Flux.just(new Signal<>(indoor.timestamp, Boolean.FALSE));
-            }
-
-            this.indoor = indoor;
-
-            return computeDeviceState(indoor, ambient);
-
-
-        } finally {
-            ThreadContext.pop();
-        }
-    }
-
-    /**
-     * Aggregate with {@link #indoor} and pass control to {@link #computeDeviceState(Signal, Signal)}.
-     *
-     * @return Empty flux if it isn't possible to compute the value, or the target device to execute.
-     */
-    private Flux<Signal<Boolean, Void>>  computeAmbient(Signal<Double, Void> ambient) {
-
-        ThreadContext.push("computeAmbient");
-
-        try {
-
-            logger.debug("ambient={}", ambient);
-
-            if (ambient.isError()) {
-
-                // Can't do much here other than shut the economizer off, who knows what's going on there
-                return Flux.just(new Signal<>(ambient.timestamp, Boolean.FALSE));
-            }
-
-            this.ambient = ambient;
-
-            return computeDeviceState(indoor, ambient);
-
-        } finally {
-            ThreadContext.pop();
-        }
-    }
-
-    private Flux<Signal<Boolean, Void>> computeDeviceState(Signal<Double, String> indoor, Signal<Double, Void> ambient) {
-
-        ThreadContext.push("computeDeviceState");
-
-        try {
-
-            logger.debug("indoor={}, ambient={}", indoor, ambient);
-
-            if (indoor == null || ambient == null) {
-
-                // One of the components is not ready yet
-                logger.debug("Just staring up? ambient or indoor are null");
-                return Flux.empty();
-            }
-
-            // The latter one wins
-            var timestamp = indoor.timestamp.isAfter(ambient.timestamp) ? indoor.timestamp : ambient.timestamp;
-
-            // VT: FIXME: This will create LOTS of jitter, don't use with units that can't deal with it
-
-            var indoorTemperature = indoor.getValue();
-            var ambientTemperature = ambient.getValue();
-
-            logger.debug("indoor={}, ambient={}", indoorTemperature, ambientTemperature);
-
-            Boolean state;
-            switch (config.mode) {
-
-                case COOLING:
-
-                    if ((ambient.getValue() < indoorTemperature - config.changeoverDelta) && indoorTemperature > config.targetTemperature) {
-                        state = Boolean.TRUE;
-                    } else {
-                        state = Boolean.FALSE;
-                    }
-                    break;
-
-                case HEATING:
-
-                    if ((ambient.getValue() > indoorTemperature + config.changeoverDelta) && indoorTemperature < config.targetTemperature) {
-                        state = Boolean.TRUE;
-                    } else {
-                        state = Boolean.FALSE;
-                    }
-                    break;
-
-                default:
-
-                    logger.error("missing config.mode??? config={}", config);
-                    state = Boolean.FALSE;
-            }
-
-            logger.debug("state={}", state);
-
-            return Flux.just(new Signal<>(timestamp, state));
-
-        } finally {
-            ThreadContext.pop();
-        }
-    }
-
-    private Signal<ZoneStatus, String> computeZone(Signal<ZoneStatus, String> source) {
-
-        if (actuatorState == null || actuatorState == Boolean.FALSE) {
-
-            // Economizer inactive, no change required
-            return source;
-        }
-
-        if (config.keepHvacOn) {
-
-            // We're feeding indoor air to HVAC air return, right?
-            return source;
-        }
-
-        if (source.isError()) {
-
-            // How did it get here? This log message is likely a dupe, need to keep an eye on it
-            logger.warn("error signal in economizer pipeline? {}", source);
-
-            // Can't do anything meaningful with it here
-            return source;
-        }
-
-        // Need to suppress demand and keep the HVAC off while the economizer is on
-        var adjusted = new ZoneStatus(
-                source.getValue().settings,
-                new ThermostatStatus(0, false));
-
-        return new Signal<>(
-                source.timestamp,
-                adjusted,
-                source.payload,
-                source.status,
-                source.error);
+        super(name, config, targetZone, ambientFlux, targetDevice);
     }
 }
