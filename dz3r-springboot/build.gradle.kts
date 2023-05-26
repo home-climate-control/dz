c109bc2a4830ce29f0e79688e65d1645fc2d8e62
Vadim Tkachenko <vt@homeclimatecontrol.com>
2023-05-25 21:53:26 -0700

    Replaced Logback with Log4J2 (#47)

--- dz3r-springboot/build.gradle.kts
+++ dz3r-springboot/build.gradle.kts
@@ -51,9 +51,15 @@ dependencies {
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-log4j2")
     runtimeOnly("io.micrometer:micrometer-registry-influx")
     runtimeOnly("io.micrometer:micrometer-registry-jmx")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
+}
+configurations {
+    all {
+        exclude("org.springframework.boot", "spring-boot-starter-logging")
+    }
 }
