675e56e24b46f3cfa412ba31828fdb2d3048f43a
Vadim Tkachenko <vt@homeclimatecontrol.com>
2023-09-19 21:46:36 -0700

    Bumped SpringBoot from 3.1.0 to 3.1.3 (at least one essential bugfix)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,7 @@ pluginManagement {
         id("org.sonarqube") version "4.0.0.2929"
         id("com.gorylenko.gradle-git-properties") version "2.4.1"
 
-        id("org.springframework.boot") version "3.1.0"
+        id("org.springframework.boot") version "3.1.3"
         id("io.spring.dependency-management") version "1.1.0"
 
         id("io.quarkus") version "3.2.0.Final"
