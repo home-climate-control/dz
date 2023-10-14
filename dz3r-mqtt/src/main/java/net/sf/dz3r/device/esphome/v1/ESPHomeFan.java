b451b627d255a742d58f41ff55b62506e0f7382e
Vadim Tkachenko <vt@homeclimatecontrol.com>
2023-10-14 01:38:57 -0700

    Introduced VariableOutputDevice, refactored out common parts (#293)

--- dz3r-mqtt/src/main/java/net/sf/dz3r/device/esphome/v1/ESPHomeFan.java
+++ dz3r-mqtt/src/main/java/net/sf/dz3r/device/esphome/v1/ESPHomeFan.java
@@ -2,6 +2,7 @@ package net.sf.dz3r.device.esphome.v1;
 
 import com.hivemq.client.mqtt.datatypes.MqttQos;
 import net.sf.dz3r.common.HCCObjects;
+import net.sf.dz3r.device.actuator.VariableOutputDevice;
 import net.sf.dz3r.device.mqtt.v1.MqttAdapter;
 import net.sf.dz3r.device.mqtt.v1.MqttSignal;
 import net.sf.dz3r.instrumentation.Marker;
@@ -23,47 +24,13 @@ import java.util.concurrent.atomic.AtomicInteger;
 /**
  * Driver for <a href="https://esphome.io/components/fan/index.html">ESPHome Fan Component</a>.
  *
- * This driver honors <a href="https://martinfowler.com/bliki/CQRS.html">Command Query Responsibility Segregation</a>.
+ * Important: {@code speed_count} needs to be left at default (100) for this driver to operate correctly.
  *
  * @author Copyright &copy; <a href="mailto:vt@homeclimatecontrol.com">Vadim Tkachenko</a> 2001-2023
  */
-public class ESPHomeFan implements AutoCloseable {
+public class ESPHomeFan implements VariableOutputDevice {
 
     private final Logger logger = LogManager.getLogger();
-
-    /**
-     * Fan device state.
-     *
-     * @param id device ID.
-     * @param available {@code true} if MQTT reported the device as available, {@code false} if not, or if it is unknown.
-     * @param requested Requested device state.
-     * @param actual Actual device state.
-     * @param queueDepth Current value of {@link #queueDepth}.
-     */
-    public record State(
-            String id,
-            boolean available,
-            FanState requested,
-            FanState actual,
-            int queueDepth
-    ) {
-
-    }
-
-    public record FanState(
-        Boolean on,
-        Double speed
-    ) {
-
-    }
-
-    private record Command(
-            boolean on,
-            double speed
-    ) {
-
-    }
-
 
     private final String id;
     private final Clock clock;
@@ -82,8 +49,8 @@ public class ESPHomeFan implements AutoCloseable {
     private final Disposable commandSubscription;
     private final Sinks.Many<Signal<State, String>> stateSink = Sinks.many().multicast().onBackpressureBuffer();
 
-    private FanState requested;
-    private FanState actual;
+    private OutputState requested;
+    private OutputState actual;
 
     public ESPHomeFan(
             String id,
@@ -167,13 +134,13 @@ public class ESPHomeFan implements AutoCloseable {
         }
     }
 
-    private FanState mergeState(FanState state, boolean on) {
+    private OutputState mergeState(OutputState state, boolean on) {
 
         if (state == null) {
-            return new FanState(on, null);
+            return new OutputState(on, null);
         }
 
-        return new FanState(on, actual.speed);
+        return new OutputState(on, actual.output());
     }
 
     private void tryParseSpeed(MqttSignal message) {
@@ -193,20 +160,16 @@ public class ESPHomeFan implements AutoCloseable {
         }
     }
 
-    private FanState mergeSpeed(FanState state, double speed) {
+    private OutputState mergeSpeed(OutputState state, double speed) {
 
         if (state == null) {
-            return new FanState(null, speed);
+            return new OutputState(null, speed);
         }
 
-        return new FanState(state.on, speed);
+        return new OutputState(state.on(), speed);
     }
 
-    /**
-     * Get the instant state of the device.
-     *
-     * @return Actual, current state of the device, immediately.
-     */
+    @Override
     public State getState() {
 
         return new State(
@@ -218,25 +181,16 @@ public class ESPHomeFan implements AutoCloseable {
         );
     }
 
-    /**
-     * Request the provided state, return immediately.
-     *
-     * If the device is not {@link #isAvailable() available}, the command is still accepted.
-     *
-     * @param on Request the device to be on, or off.
-     * @param speed Request the speed, {@code 0.0} to {@code 1.0}.
-     *
-     * @return The result of {@link #getState()} after the command was accepted (not executed).
-     */
-    public synchronized State setState(boolean on, double speed) {
+    @Override
+    public synchronized State setState(boolean on, double output) {
 
-        if (speed < 0 || speed > 1) {
-            throw new IllegalArgumentException("speed given (" + speed + ") is outside of 0..1 range");
+        if (output < 0 || output > 1) {
+            throw new IllegalArgumentException("speed given (" + output + ") is outside of 0..1 range");
         }
 
-        this.requested = new FanState(on, speed);
+        this.requested = new OutputState(on, output);
         queueDepth.incrementAndGet();
-        commandSink.tryEmitNext(new Command(on, speed));
+        commandSink.tryEmitNext(new Command(on, output));
 
         var state = getState();
         stateSink.tryEmitNext(new Signal<>(Instant.now(clock), state, id));
@@ -258,8 +212,8 @@ public class ESPHomeFan implements AutoCloseable {
 
             // This will translate into two commands, but so be it
 
-            adapter.publish(commandTopic, command.on ? "ON" : "OFF", MqttQos.AT_LEAST_ONCE, false);
-            adapter.publish(speedCommandTopic, Integer.toString((int) (command.speed * 100)), MqttQos.AT_LEAST_ONCE, false);
+            adapter.publish(commandTopic, command.on() ? "ON" : "OFF", MqttQos.AT_LEAST_ONCE, false);
+            adapter.publish(speedCommandTopic, Integer.toString((int) (command.output() * 100)), MqttQos.AT_LEAST_ONCE, false);
 
             queueDepth.decrementAndGet();
 
@@ -271,23 +225,12 @@ public class ESPHomeFan implements AutoCloseable {
         }
     }
 
-    /**
-     * Check the reported device availability.
-     *
-     * @return previously reported device availability, immediately.
-     */
+    @Override
     public boolean isAvailable() {
         return "online".equals(availabilityMessage);
     }
 
-    /**
-     * Get the state change notification flux.
-     *
-     * Note that this sink will NOT emit error signals, however, {@link State#available} will reflect device availability
-     * the moment the signal was emitted.
-     *
-     * @return Flux emitting signals every time the {@link #setState(boolean, double) requested} or {@link #getState() actual} state has changed.
-     */
+    @Override
     public Flux<Signal<State, String>> getFlux() {
         return stateSink.asFlux();
     }
@@ -296,9 +239,6 @@ public class ESPHomeFan implements AutoCloseable {
         return new Signal<>(Instant.now(clock), getState(), id);
     }
 
-    /**
-     * Close the device, synchronously.
-     */
     @Override
     public void close() throws Exception {
 
