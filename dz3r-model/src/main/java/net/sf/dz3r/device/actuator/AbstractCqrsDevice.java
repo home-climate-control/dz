a83e0026796eb12e4c54efd0033fbc1ad1396de2
Vadim Tkachenko <vt@homeclimatecontrol.com>
2023-10-27 23:37:16 -0700

    Implemented rate limiting for CQRS compliant devices (#292)

--- dz3r-model/src/main/java/net/sf/dz3r/device/actuator/AbstractCqrsDevice.java
+++ dz3r-model/src/main/java/net/sf/dz3r/device/actuator/AbstractCqrsDevice.java
@@ -5,6 +5,7 @@ import net.sf.dz3r.device.DeviceState;
 import net.sf.dz3r.signal.Signal;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.ThreadContext;
 import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Sinks;
@@ -12,6 +13,7 @@ import reactor.core.scheduler.Schedulers;
 
 import java.time.Clock;
 import java.time.Duration;
+import java.time.Instant;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public abstract class AbstractCqrsDevice<I, O> implements CqrsDevice<I, O> {
@@ -38,6 +40,9 @@ public abstract class AbstractCqrsDevice<I, O> implements CqrsDevice<I, O> {
     private final Disposable commandSubscription;
     protected final Sinks.Many<Signal<DeviceState<O>, String>> stateSink = Sinks.many().multicast().onBackpressureBuffer();
 
+    private I lastCommand;
+    private Instant lastSet;
+
     protected O requested;
     protected O actual;
 
@@ -53,9 +58,49 @@ public abstract class AbstractCqrsDevice<I, O> implements CqrsDevice<I, O> {
         commandSubscription = commandSink
                 .asFlux()
                 .publishOn(Schedulers.newSingle("cqrs-" + id))
+                .flatMap(this::limitRate)
                 .subscribe(this::setStateSync);
     }
 
+    /**
+     * Make sure {@link #setStateSync(Object)} is not called for identical commands more often than {@link #pace} interval in between.
+     *
+     * @param command Command to inspect
+     *
+     * @return Flux of just the input command if the pace is not exceeded, or empty Flux if it is.
+     */
+    synchronized Flux<I> limitRate(I command) {
+
+        ThreadContext.push("limitRate");
+
+        try {
+
+            var now = clock.instant();
+
+            if (!command.equals(lastCommand)) {
+
+                logger.trace("{}: command={} pass - different from {}", id, command, lastCommand);
+
+                lastCommand = command;
+                lastSet = now;
+
+                return Flux.just(command);
+            }
+
+            var interval = Duration.between(lastSet, now);
+            if (interval.compareTo(pace) < 0) {
+                logger.trace("{}: command={} drop - too soon ({} vs {})", id, command, interval, pace);
+                return Flux.empty();
+            }
+
+            logger.trace("{}: command={} pass - {} is beyond {}", id, command, interval, pace);
+            lastSet = now;
+            return Flux.just(command);
+
+        } finally {
+            ThreadContext.pop();
+        }
+    }
 
     @Override
     public DeviceState<O> getState() {
