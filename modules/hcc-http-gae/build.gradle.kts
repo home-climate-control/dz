dd664de43f91c3f96ab65ed549c590e0dec2b689
Vadim Tkachenko <vt@homeclimatecontrol.com>
2024-03-12 20:03:46 -0700

    Killed Gson (#307)

--- modules/hcc-http-gae/build.gradle.kts
+++ modules/hcc-http-gae/build.gradle.kts
@@ -4,9 +4,6 @@ plugins {
 
 dependencies {
 
-    // https://cloud.google.com/docs/authentication/production#auth-cloud-implicit-java
-    // VT: FIXME: The only thing necessary from here is Gson, it can be replaced with Jackson here
-    implementation(libs.google.cloud.storage)
     implementation(libs.jackson.databind)
 
     implementation(project(":modules:hcc-common"))
