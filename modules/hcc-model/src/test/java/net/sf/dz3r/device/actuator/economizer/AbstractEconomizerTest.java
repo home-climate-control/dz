6074fb812ee0deeeb976c79f18d4838870f4f01a
Vadim Tkachenko <vt@homeclimatecontrol.com>
2024-11-12 20:12:04 -0700

    Extended test case to heating mode (#328)

--- modules/hcc-model/src/test/java/net/sf/dz3r/device/actuator/economizer/AbstractEconomizerTest.java
+++ modules/hcc-model/src/test/java/net/sf/dz3r/device/actuator/economizer/AbstractEconomizerTest.java
@@ -20,11 +20,11 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 class AbstractEconomizerTest {
 
     /**
-     * Make sure that control signal is computed properly as the indoor temperature is approaching the {@link EconomizerSettings#targetTemperature()}.
+     * Make sure that control signal is computed properly in cooling mode as the indoor temperature is approaching the {@link EconomizerSettings#targetTemperature()}.
      */
     @ParameterizedTest
-    @MethodSource("targetAdjustmentProvider")
-    void targetAdjustmentTest(TargetAdjustmentTestData source) {
+    @MethodSource("targetAdjustmentCoolingProvider")
+    void targetAdjustmentCoolingTest(TargetAdjustmentTestData source) {
 
         var config = new EconomizerConfig(
                 source.mode,
@@ -38,12 +38,48 @@ class AbstractEconomizerTest {
         );
 
         var e = new TestEconomizer(
-                "eco",
+                "eco-cool",
                 config,
                 new SwitchableHvacDevice(
                         Clock.systemUTC(),
-                        "d",
+                        "cooler",
                         HvacMode.COOLING,
+                        new NullCqrsSwitch("s"),
+                        false,
+                        null)
+        );
+
+        var signal = e.computeCombined(source.indoorTemperature, source.ambientTemperature);
+
+        assertThat(signal).isEqualTo(source.expectedSignal);
+
+    }
+
+    /**
+     * Make sure that control signal is computed properly in heating mode as the indoor temperature is approaching the {@link EconomizerSettings#targetTemperature()}.
+     */
+    @ParameterizedTest
+    @MethodSource("targetAdjustmentHeatingProvider")
+    void targetAdjustmentHeatingTest(TargetAdjustmentTestData source) {
+
+        var config = new EconomizerConfig(
+                source.mode,
+                -1.0, -0.0001, 1.0,
+                new EconomizerSettings(
+                        source.changeoverDelta,
+                        source.targetTemperature,
+                        true,
+                        1.0
+                )
+        );
+
+        var e = new TestEconomizer(
+                "eco-heat",
+                config,
+                new SwitchableHvacDevice(
+                        Clock.systemUTC(),
+                        "heater",
+                        HvacMode.HEATING,
                         new NullCqrsSwitch("s"),
                         false,
                         null)
@@ -100,9 +136,9 @@ class AbstractEconomizerTest {
     }
 
     /**
-     * @return Stream of {@link TargetAdjustmentTestData} for {@link #targetAdjustmentTest(TargetAdjustmentTestData)}.
+     * @return Stream of {@link TargetAdjustmentTestData} for {@link #targetAdjustmentCoolingTest(TargetAdjustmentTestData)}.
      */
-    private static Stream<TargetAdjustmentTestData> targetAdjustmentProvider() {
+    private static Stream<TargetAdjustmentTestData> targetAdjustmentCoolingProvider() {
 
         return Stream.of(
                 new TargetAdjustmentTestData(HvacMode.COOLING, 1.0, 22.0, 25.0, 10.0, 14.0),
@@ -119,7 +155,25 @@ class AbstractEconomizerTest {
                 // NEGATIVE_INFINITY is wrong, but this is what it is now. Will be adjusted after the fix is in.
                 new TargetAdjustmentTestData(HvacMode.COOLING, 0.0, 22.0, 21.0, 20.0, Double.NEGATIVE_INFINITY)
         );
-
     }
 
+    /**
+     * @return Stream of {@link TargetAdjustmentTestData} for {@link #targetAdjustmentHeatingTest(TargetAdjustmentTestData)}.
+     */
+    private static Stream<TargetAdjustmentTestData> targetAdjustmentHeatingProvider() {
+
+        return Stream.of(
+                new TargetAdjustmentTestData(HvacMode.HEATING, 1.0, 25.0, 23.0, 30.0, 6.0),
+                new TargetAdjustmentTestData(HvacMode.HEATING, 1.0, 25.0, 24.0, 30.0, 5.0),
+                new TargetAdjustmentTestData(HvacMode.HEATING, 1.0, 25.0, 24.5, 30.0, 2.25),
+                new TargetAdjustmentTestData(HvacMode.HEATING, 1.0, 25.0, 25.0, 30.0, 0.0),
+                new TargetAdjustmentTestData(HvacMode.HEATING, 1.0, 25.0, 25.5, 30.0, -1.75),
+                new TargetAdjustmentTestData(HvacMode.HEATING, 1.0, 25.0, 26.0, 30.0, -3.0),
+
+                // https://github.com/home-climate-control/dz/issues/328
+                // Note changeoverDelta == 0.
+                // NEGATIVE_INFINITY is wrong, but this is what it is now. Will be adjusted after the fix is in.
+                new TargetAdjustmentTestData(HvacMode.HEATING, 0.0, 25.0, 26.0, 30.0, Double.NEGATIVE_INFINITY)
+        );
+    }
 }
