df17f1738da15088f5783947f174e7165adbdf0d
Vadim Tkachenko <vt@homeclimatecontrol.com>
2025-10-08 18:30:32 -0700

    Fixed the Quarkus side of the sensitivity configuration (#322)

--- app/hcc-quarkus/src/main/java/net/sf/dz3r/runtime/config/quarkus/model/HalfLifeConfig.java
+++ app/hcc-quarkus/src/main/java/net/sf/dz3r/runtime/config/quarkus/model/HalfLifeConfig.java
@@ -1,8 +1,6 @@
 package net.sf.dz3r.runtime.config.quarkus.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
 import java.time.Duration;
 import java.util.Optional;
@@ -10,16 +8,19 @@ import java.util.Optional;
 /**
  * {@link net.sf.dz3r.controller.HalfLifeController} configuration.
  *
- * @param halfLife See <a href="https://www.omnicalculator.com/chemistry/half-life#half-life-formula">half life formulas</a> for explanation.
- * @param multiplier Multiply the standard {@link net.sf.dz3r.controller.HalfLifeController} output signal by this to obtain the effective output value. Must be positive.
- *
- * @author Copyright &copy; <a href="mailto:vt@homeclimatecontrol.com">Vadim Tkachenko</a> 2001-2024
+ * @author Copyright &copy; <a href="mailto:vt@homeclimatecontrol.com">Vadim Tkachenko</a> 2001-2025
  */
-@JsonNaming(PropertyNamingStrategies.KebabCaseStrategy.class)
-public record HalfLifeConfig(
-        @JsonProperty("half-life")
-        Optional<Duration> halfLife,
-        @JsonProperty("multiplier")
-        Optional<Double> multiplier
-) {
+public interface HalfLifeConfig {
+
+    /**
+     * See <a href="https://www.omnicalculator.com/chemistry/half-life#half-life-formula">half life formulas</a> for explanation.
+     */
+    @JsonProperty("half-life")
+    Optional<Duration> halfLife();
+
+    /**
+     * Multiply the standard {@link net.sf.dz3r.controller.HalfLifeController} output signal by this to obtain the effective output value. Must be positive.
+     */
+    @JsonProperty("multiplier")
+    Optional<Double> multiplier();
 }
