bfd7d8368dbee2fb8daea41b9bd5380323ecde4e
Vadim Tkachenko <vt@homeclimatecontrol.com>
2026-04-22 18:57:11 -0700

    Figured out the relation between changeover-delta and enthalpy (#348)

--- modules/hcc-common/src/test/java/com/homeclimatecontrol/hcc/hvac/PsychrometricsToolTest.java
+++ modules/hcc-common/src/test/java/com/homeclimatecontrol/hcc/hvac/PsychrometricsToolTest.java
@@ -5,21 +5,59 @@ import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
 
 import static com.homeclimatecontrol.hcc.hvac.PsychrometricsTool.STANDARD_ATMOSPHERIC_PRESSURE;
+import static com.homeclimatecontrol.hcc.hvac.PsychrometricsTool.calculateEnthalpy;
+import static com.homeclimatecontrol.hcc.hvac.PsychrometricsTool.getAirSpecificHeat;
 
 class PsychrometricsToolTest {
 
     private final Logger logger = LogManager.getLogger();
 
     @Test
-    void printEnthalpy() {
+    void printEnthalpyDiff() {
 
-        var temperature = 25;
+        // https://github.com/home-climate-control/dz/issues/348
+        //
+        // Good luck explaining to people what enthalpy is so that they can configure their economizer's
+        // changeover-delta (which is already mysterious enough). Let's see how that translates into human readable terms.
+        // Let's also print this as a CSV so that it can be imported and charted.
 
-        for (var rh = 10; rh <= 100; rh += 10) {
-            logger.info("Enthalpy at temp={}°C rh={}%: {}",
-                    temperature,
-                    rh,
-                    PsychrometricsTool.calculateEnthalpy(25, rh / 100.0, STANDARD_ATMOSPHERIC_PRESSURE));
+        // Based on these charts, it looks like doubling the changeover-delta will yield a reasonable approximation.
+
+        printEnthalpyDiffByTemperature();
+        printEnthalpyDiffByHumidity();
+    }
+
+    void printEnthalpyDiffByTemperature() {
+
+        logger.info("temperature,enthalpy at 50% RH,delta");
+
+        Double trailer = null;
+
+        for (var t = 15; t <= 30; t++) {
+            var e = calculateEnthalpy(t, 0.5, STANDARD_ATMOSPHERIC_PRESSURE);
+
+            if (trailer != null) {
+                logger.info("{},{},{}", t, e, e - trailer);
+            }
+
+            trailer = e;
+        }
+    }
+
+    void printEnthalpyDiffByHumidity() {
+
+        logger.info("humidity at 25°C,enthalpy,delta");
+
+        Double trailer = null;
+
+        for (var rh = 10; rh <= 100; rh += 5) {
+            var e = calculateEnthalpy(25, rh / 100.0, STANDARD_ATMOSPHERIC_PRESSURE);
+
+            if (trailer != null) {
+                logger.info("{},{},{}", rh, e, e - trailer);
+            }
+
+            trailer = e;
         }
     }
 
@@ -27,7 +65,7 @@ class PsychrometricsToolTest {
     void printAirSpecificHeat() {
 
         for (var t = 0; t < 100; t++) {
-            logger.info("specific heat of air at {}°C: {}", t, PsychrometricsTool.getAirSpecificHeat(t));
+            logger.info("specific heat of air at {}°C: {}", t, getAirSpecificHeat(t));
         }
     }
 }
