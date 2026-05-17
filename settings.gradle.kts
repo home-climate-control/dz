f8598957e5047653e7adc98b97af16d4e7c016bd
Vadim Tkachenko <vt@homeclimatecontrol.com>
2026-05-16 21:47:04 -0700

    Dodged build failures with latest Gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,7 +15,7 @@ dependencyResolutionManagement {
 
 plugins {
     // Apply the foojay-resolver plugin to allow automatic download of JDKs
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
 rootProject.name = "dz3-master"
